d96a91813f96663e869d8fcbcad307916bcfb1f7
Rhenaldo Arya <rhenaldoaryap@gmail.com>
2022-03-04 21:12:56 +0700

    refactoring the CSRF token handling

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const db = require("./data/database");
 const authRoutes = require("./routes/auth");
 const blogRoutes = require("./routes/blog");
 const authMiddleware = require("./middlewares/auth-middleware");
+const addCSRFTokenMiddleware = require("./middlewares/csrf-token-middleware");
 
 // calling the createSessionStore function from config folder/session.js and passing session package as argument.
 const mongoDbSessionStore = sessionConfig.createSessionStore(session);
@@ -23,6 +24,9 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(session(sessionConfig.createSessionConfig(mongoDbSessionStore)));
 app.use(csrf());
+
+// add custom middleware that hold the CSRF token
+app.use(addCSRFTokenMiddleware);
 
 // not executing our custom middleware, we let the package (express) execute it for us when the request coming in the future
 // that means the res.locals
